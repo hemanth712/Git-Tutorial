f18f3462e3a09c344739c2c5fe5e44c286862e0f
Hemanth Muduganti <m_hemanth7@yahoo.co.in>
2022-04-09 14:06:44 +0530

    Version 1

--- StaticInitializer.cpp
+++ StaticInitializer.cpp
@@ -5,17 +5,20 @@ class HasStatic {
     static int x, y;
     public:
     void print() {
+
         cout<<"HasStatic::x"<<x<<endl;
         cout<<"HasStatic::y"<<y<<endl;
 
     }
        
-}
+};
 
-   int HasStatic::x = 1;
-   int HasStatic::y = x+1;
+int HasStatic::x = 1;
+int HasStatic::y = x+1;
 
 void main() {
+
     HasStatic HS;
     HS.print();
+    
 } 
