6443c2d80513662633bdd53a9df6f814405a04b4
Hemanth Muduganti <m_hemanth7@yahoo.co.in>
2022-04-09 14:33:44 +0530

    Version 3

--- Enum.cpp
+++ Enum.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-// Enumerations Version 2
+// Enumerations Version 3
 void main() {
 	enum WeekDay[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 	cout << "WeekDays" << endl<<WeekDay;
